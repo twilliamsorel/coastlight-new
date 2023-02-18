8fec70b2a25ff3083ca458aafd4bf2f5ad3d4555
Trevor Sorel <twilliamsorel@gmail.com>
2023-02-18 13:50:18 -0700

    enabling css compression

--- _src/_data/globals.js
+++ _src/_data/globals.js
@@ -1,5 +1,23 @@
+const fs = require("fs/promises");
+const path = require("path");
+const CleanCSS = require("clean-css");
+
 module.exports = function() {
+  async () => {
+    await fs
+      .readFile(path.resolve(__dirname, "../assets/css/main.css"))
+      .then((data) => { 
+        let minifiedCSS = new CleanCSS().minify(data).styles
+        fs.writeFile('_src/assets/css/main.min.css', minifiedCSS, (err) => {
+          console.log(err);
+        })
+      })
+  };
+
   return {
-    environment: process.env.ENVIRONMENT || "development"
+    environment: process.env.ENVIRONMENT || "development",
+    cssPath: process.env.ENVIRONMENT === 'production' ? '/assets/css/main.min.css' : '/assets/css/main.css',
+    "title": "Coastlight Digital",
+    "Description": "The marketing website of the digital agency Coastlight Digital."
   };
 };
