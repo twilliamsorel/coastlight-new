9025726b8243ea1da7e2d1650932f48463147fd9
Trevor Sorel <twilliamsorel@gmail.com>
2023-02-23 16:11:20 -0700

    refactoring js asset handling a bit

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,5 +1,10 @@
 import mainMenuScrolling, { toggleMobileMenu } from './mainMenus.js';
+import sliderAnimator from './slider.js'
 
 // INITIALIZING MAIN NAVIGATION JS
 mainMenuScrolling();
 toggleMobileMenu();
+
+if (window.location.pathname.match('/web-design/$')) {
+  sliderAnimator();
+}
--- _src/assets/js/slider.js
+++ _src/assets/js/slider.js
@@ -1,28 +1,29 @@
-const slideContainer = document.querySelector('.scroll-container');
+export default function sliderAnimator() {
+  const slideContainer = document.querySelector('.scroll-container');
 
-let startCoord;
-let currentOffset = 0;
+  let startCoord;
+  let currentOffset = 0;
 
-// DESKTOP
-slideContainer.addEventListener('mousedown', function (e) {
-  console.log(e);
-  startCoord = e.clientX;
-  currentOffset = slideContainer.scrollLeft;
-});
+  // DESKTOP
+  slideContainer.addEventListener('mousedown', function (e) {
+    startCoord = e.clientX;
+    currentOffset = slideContainer.scrollLeft;
+  });
 
-slideContainer.addEventListener('mouseup', function (e) {
-  currentOffset = slideContainer.scrollLeft;
-  startCoord = null;
-});
+  slideContainer.addEventListener('mouseup', function (e) {
+    currentOffset = slideContainer.scrollLeft;
+    startCoord = null;
+  });
 
-window.addEventListener('mouseout', function () {
-  currentOffset = slideContainer.scrollLeft;
-  startCoord = null;
-})
+  window.addEventListener('mouseout', function () {
+    currentOffset = slideContainer.scrollLeft;
+    startCoord = null;
+  })
 
-slideContainer.addEventListener('mousemove', function (e) {
-  if (startCoord) {
-    dynamicOffset = currentOffset + (startCoord - e.clientX);
-    slideContainer.scrollLeft = dynamicOffset;
-  }
-});
+  slideContainer.addEventListener('mousemove', function (e) {
+    if (startCoord) {
+      let dynamicOffset = currentOffset + (startCoord - e.clientX);
+      slideContainer.scrollLeft = dynamicOffset;
+    }
+  });
+}
