93e69e34db48ffff922cb4278b312e6be5567a00
Trevor Sorel <twilliamsorel@gmail.com>
2023-03-17 14:35:58 -0600

    fix min generation?

--- _src/_data/globals.js
+++ _src/_data/globals.js
@@ -10,22 +10,22 @@ module.exports = async function () {
       .readFile(path.resolve(__dirname, "../assets/css/main.css"))
       .then((data) => {
         let minifiedCSS = new CleanCSS().minify(data).styles;
-        fs.writeFile('_src/assets/css/main.min.css', minifiedCSS, (err) => {
+        fs.writeFile('_src/assets/css/main-min.css', minifiedCSS, (err) => {
           console.log(err);
         });
       });
 
     let data = await fs.readFile(path.resolve(__dirname, "../assets/js/main.js"), 'utf-8');
     let minifiedJS = await terser.minify(data);
-    fs.writeFile('_src/assets/js/main.min.js', minifiedJS.code, (err) => {
+    fs.writeFile('_src/assets/js/main-min.js', minifiedJS.code, (err) => {
       console.log(err);
     });
   }
 
   return {
     environment: process.env.ENVIRONMENT || "development",
-    cssPath: process.env.ENVIRONMENT === 'production' ? '/assets/css/main.min.css' : '/assets/css/main.css',
-    jsPath: process.env.ENVIRONMENT === 'production' ? '/assets/css/main.min.js' : '/assets/js/main.js',
+    cssPath: process.env.ENVIRONMENT === 'production' ? '/assets/css/main-min.css' : '/assets/css/main.css',
+    jsPath: process.env.ENVIRONMENT === 'production' ? '/assets/css/main-min.js' : '/assets/js/main.js',
     "title": "Coastlight Digital",
     "Description": "The marketing website of the digital agency Coastlight Digital."
   };
