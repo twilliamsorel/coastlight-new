b374122bfb866936c341fe876ef4095f0c7f8585
Trevor Sorel <twilliamsorel@gmail.com>
2023-03-21 10:30:35 -0600

    attempting to fix prod build

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,8 @@
+const fs = require("fs/promises");
+const path = require("path");
+const CleanCSS = require("clean-css");
+const terser = require("terser");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("_src/assets");
   eleventyConfig.addPassthroughCopy("_src/favicon-32x32.png");
@@ -6,6 +11,26 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.setLiquidOptions({
     dynamicPartials: false,
     strict_filters: true
+  });
+
+  // MINIFYING CSS AND JS
+  eleventyConfig.on('eleventy.before', async () => {
+    if (process.env.ENVIRONMENT === 'production') {
+      await fs
+        .readFile(path.resolve(__dirname, "_src/assets/css/main.css"))
+        .then((data) => {
+          let minifiedCSS = new CleanCSS().minify(data).styles;
+          fs.writeFile('_src/assets/css/main-min.css', minifiedCSS, (err) => {
+            console.log(err);
+          });
+        });
+
+      let data = await fs.readFile(path.resolve(__dirname, "_src/assets/js/main.js"), 'utf-8');
+      let minifiedJS = await terser.minify(data);
+      fs.writeFile('_src/assets/js/main-min.js', minifiedJS.code, (err) => {
+        console.log(err);
+      });
+    }
   });
 
   return {
--- _src/_data/globals.js
+++ _src/_data/globals.js
@@ -1,27 +1,4 @@
-const fs = require("fs/promises");
-const path = require("path");
-const CleanCSS = require("clean-css");
-const terser = require("terser");
-
 module.exports = async function () {
-  // MINIFYING CSS AND JS
-  if (process.env.ENVIRONMENT === 'production') {
-    await fs
-      .readFile(path.resolve(__dirname, "../assets/css/main.css"))
-      .then((data) => {
-        let minifiedCSS = new CleanCSS().minify(data).styles;
-        fs.writeFile('_src/assets/css/main-min.css', minifiedCSS, (err) => {
-          console.log(err);
-        });
-      });
-
-    let data = await fs.readFile(path.resolve(__dirname, "../assets/js/main.js"), 'utf-8');
-    let minifiedJS = await terser.minify(data);
-    fs.writeFile('_src/assets/js/main-min.js', minifiedJS.code, (err) => {
-      console.log(err);
-    });
-  }
-
   return {
     environment: process.env.ENVIRONMENT || "development",
     cssPath: process.env.ENVIRONMENT === 'production' ? '/assets/css/main-min.css' : '/assets/css/main.css',
